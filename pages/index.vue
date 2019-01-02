c0617289c92c5ea194f0ef0d79ec6c892c50e89c
ecouffes <tanakakatsunori2@gmail.com>
2019-01-02 14:15:41 +0900

    fix for private network

--- pages/index.vue
+++ pages/index.vue
@@ -72,10 +72,9 @@
   import Eos from 'eosjs'
   import ScatterJS from 'scatter-js/dist/scatter.esm'
 
-  // const CONTRACT_ACCOUNT = 'addressbook'
-  // const USER_ACCOUNT = 'bob'
-  const CONTRACT_ACCOUNT = 'eosaddressbk'
-  const USER_ACCOUNT = 'eosaddressbk'
+  // Private network account
+  const CONTRACT_ACCOUNT = 'addressbook'
+  const USER_ACCOUNT = 'bob'
 
   export default {
     data () {
@@ -131,24 +130,18 @@
     },
 
     async asyncData (context) {
-      // const network = {
-      //   blockchain: 'eos',
-      //   protocol: 'http',
-      //   host: '0.0.0.0',
-      //   port: 7777,
-      //   chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
-      // }
+
+      // Private Network info
       const network = {
         blockchain: 'eos',
-        protocol: 'https',
-        host: 'api-kylin.eoslaomao.com',
-        port: 443,
-        chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191'
+        protocol: 'http',
+        host: '0.0.0.0',
+        port: 7777,
+        chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
       }
 
       // ScatterにLink
-      // const connected = await ScatterJS.scatter.connect('EOS Addressbook')
-      const connected = await ScatterJS.scatter.connect('EOS Kylin Addressbook')
+      const connected = await ScatterJS.scatter.connect('EOS Addressbook')
       if (!connected) return false
 
       const scatter = ScatterJS.scatter
@@ -251,7 +244,6 @@
           this.modalContact.tel,
           { authorization: `${this.account.name}@owner` }
         ).then(result => {
-          // トランザクション送信成功
           this.warning = 'トランザクションを送信しました'
         }).then(async () => {
           this.refreshcontracts()
